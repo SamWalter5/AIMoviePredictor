0f08493462cf375bc81ecb3facef40b028c7b87a
Samantha Walter <walters2023@fau.edu>
2025-11-25 14:07:10 -0500

    Initial Commit

---
+++ AIMoviePredictor/AIMoviePredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIMoviePredictorApp.swift
+//  AIMoviePredictor
+//
+//  Created by Samantha Walter on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct AIMoviePredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIMoviePredictorTests/AIMoviePredictorTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIMoviePredictorTests.swift
+//  AIMoviePredictorTests
+//
+//  Created by Samantha Walter on 11/25/25.
+//
+
+import Testing
+@testable import AIMoviePredictor
+
+struct AIMoviePredictorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
